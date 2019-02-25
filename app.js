3cf76e9a4c0d4768f5c87524b1e0413ee859a37a
Andrei-Zhuk <zhuk_andrey@tut.by>
2019-02-25 09:58:30 +0300

    implement tasks 1-4

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const uniqid = require('uniqid');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
 
 const app = express();
 
@@ -9,13 +12,86 @@ app.use(queryParser);
 app.use(bodyParser.json());
 
 const products = [];
-const users = [];
+const users = [
+    {
+        username: "Clark Kent",
+        email: "clark_kent@gmail.com",
+        password: "1234",
+    }
+];
 
-app.get('/api/products', (req, res) => {
+passport.use(new LocalStrategy({
+    usernameField: "email",
+    passwordField: "password",
+    session: false,
+}, (username, password, done) => {
+    const user = users.find((user) => user.email === username);
+
+    if (user === undefined || user.password !== password) {
+        done(null, false, "bad username")
+    } else {
+        done(null, user)
+    }
+}))
+
+function checkToken(req, res, next) {
+    const token = req.headers["x-access-token"];
+
+    if (token) {
+        jwt.verify(token, "secret word", function(err) {
+            if (err) {
+                res.status(403).send({code: 403, message: "Forbidden", data: {}});
+            } else {
+                next()
+            }
+        });
+    } else {
+        res.status(403).send({code: 403, message: "No token provided", data: {}});
+    }
+}
+
+app.post('/auth', passport.authenticate("local", {session: false}), (req, res) => {
+    // const {email, password} = req.body;
+    // const user = users.find((user) => user.email === email);
+
+    // if (user === undefined || user.password !== password) {
+    //     res.status(404).send({code: 404, message: "Not Found", data: {}})
+    // } else {
+    //     const token = jwt.sign({id: user.username}, "secret word", { expiresIn: 100 });
+    //     const response = {
+    //         code: 200,
+    //         message: "OK",
+    //         data: {
+    //             user: {
+    //                 email: user.email,
+    //                 username: user.username,
+    //             }
+    //         },
+    //         token,
+    //     }
+    //     res.json(response);
+    // }
+
+    const token = jwt.sign({id: req.user.username}, "secret word", { expiresIn: 100 });
+    const response = {
+        code: 200,
+        message: "OK",
+        data: {
+            user: {
+                email: req.user.email,
+                username: req.user.username,
+            }
+        },
+        token,
+    }
+    res.json(response);
+})
+
+app.get('/api/products', checkToken, (req, res) => {
     res.json(JSON.stringify(products));
 });
   
-app.get('/api/products/:id', (req, res) => {
+app.get('/api/products/:id', checkToken, (req, res) => {
     for (let i = 0; i < products.length; i++) {
         if (products[i].id === req.params.id) {
             res.json(JSON.stringify(products[i]));
@@ -25,7 +101,7 @@ app.get('/api/products/:id', (req, res) => {
     res.send(`product ${req.params.id} doesnt exist`);
 });
 
-app.get('/api/products/:id/reviews', (req, res) => {
+app.get('/api/products/:id/reviews', checkToken, (req, res) => {
     for (let i = 0; i < products.length; i++) {
         if (products[i].id === req.params.id) {
             res.json(JSON.stringify(products[i].reviews));
@@ -35,7 +111,7 @@ app.get('/api/products/:id/reviews', (req, res) => {
     res.send(`product ${req.params.id} doesnt exist`);
 });
   
-app.post('/api/products', (req, res) => {
+app.post('/api/products', checkToken, (req, res) => {
     console.log(req.body)
     let product = {
         ...req.body,
@@ -45,12 +121,17 @@ app.post('/api/products', (req, res) => {
     res.json(JSON.stringify(product));
 })
 
-app.get('/api/users', (req, res) => {
-    res.json(JSON.stringify(users));
+app.get('/api/users', checkToken, (req, res) => {
+    res.json(users);
 })
 
 function cookieParser(req, res, next) {
     let parsedCookies = {};
+    if (!req.headers.cookie) {
+        req.parsedCookies = parsedCookies;
+        next();
+        return;
+    }
     let cookies = req.headers.cookie.split(";")
     for (let i = 0; i < cookies.length; i++) {
         let cookieFields = cookies[i].split("=");
